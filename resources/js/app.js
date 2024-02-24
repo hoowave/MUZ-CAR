0e689eb9e21ce678993610a6b809105fdd0c54ba
hoowave <ma5ter@naver.com>
2024-02-24 15:54:47 +0900

    tailwind

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import '../css/app.css';
+
+createInertiaApp({
+  resolve: name => import(`./components/${name}.vue`)
+    .then(module => module.default),
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el);
+  },
+});
---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Your App</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    @inertia
+</body>
+</html>
